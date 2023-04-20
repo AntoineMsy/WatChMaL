added correct normalization

watchmal/dataset/cnn_mpmt/cnn_mpmt_dataset.py
@@ -10,6 +10,7 @@ import torch.nn.functional as F
 # generic imports
 import numpy as np
 import torch
+from sklearn.preprocessing import MinMaxScaler
 
 # WatChMaL imports
 from watchmal.dataset.h5_dataset import H5Dataset
@@ -58,7 +59,8 @@ class CNNmPMTDataset(H5Dataset):
        
         self.collapse_arrays = collapse_arrays
         self.transforms = du.get_transformations(self, transforms)
- 
+        self.scaler = MinMaxScaler((-1,1))
+
         if padding_type is not None:
             self.padding_type = getattr(self, padding_type)
         else:
@@ -95,7 +97,7 @@ class CNNmPMTDataset(H5Dataset):
         # fix barrel array indexing to match endcaps in xyz ordering
         barrel_data = data[:, self.barrel_rows, :]
         data[:, self.barrel_rows, :] = barrel_data[barrel_map_array_idxs, :, :]
-        
+        data = self.scaler.fit_transform(data.reshape(-1, data.shape[-1])).reshape(data.shape)
         # collapse arrays if desired
         if self.collapse_arrays:
             data = np.expand_dims(np.sum(data, 0), 0)

watchmal/engine/engine_sae.py
@@ -161,12 +161,14 @@ class AutoEncoderEngine:
         with torch.set_grad_enabled(train):
             # Move the data and the labels to the GPU (if using CPU this has no effect)
             data = self.data.to(self.device)
+            print(torch.max(data))
             labels = self.labels.to(self.device)
             y_onehot = torch.FloatTensor(self.batch_size, self.model.num_classes).to(self.device)
             y_onehot.zero_()
             cond_x = y_onehot.scatter(1, labels.reshape([-1,1]),1).to(self.device)
             self.model.zero_grad()
             autoencoder_output, y = self.model(data)
+            print(torch.max(autoencoder_output))
             loss_mse = self.criterion(autoencoder_output,data) #use MSE
 
             rand_x = torch.rand(self.batch_size, self.model.input_size).to(self.device) ### Generate input noise for the noise generator
@@ -181,7 +183,7 @@ class AutoEncoderEngine:
                 print( " ... ")
                 print(data.size)
                 print(labels.size)
-                
+
             self.loss = self.noise_gen_weight*ng_loss+ self.reconstruction_weight*loss_mse
             
 